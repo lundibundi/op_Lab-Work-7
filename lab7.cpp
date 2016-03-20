6ca76ca942f73c917667ca2c8da0dad52ba9541f
lundibundi <shishugi@gmail.com>
2016-03-20 12:05:19 +0200

    implement parsing and refactoring

--- lab7.cpp
+++ lab7.cpp
@@ -6,7 +6,50 @@
 #include <conio.h>
 #include <iostream>
 
-char* read_input()
+char* read_line();
+void parse_string(char* input, const char& beg, const char& end);
+
+int lab7()
+{
+	// read input
+	char beg, end;
+	std::cout << "enter beginning character: ";
+	std::cin >> beg;
+	std::cout << "enter ending character: ";
+	std::cin >> end;
+	std::cout << "enter string: ";
+
+	// ignore all that's left after previous input
+	std::cin.ignore(INT_MAX, '\n');
+	char* input = read_line();
+
+	// parse string
+	parse_string(input, beg, end);
+
+	delete[] input;
+	_getch();
+	return 0;
+}
+
+void parse_string(char* input, const char& beg, const char& end)
+{
+	int len = strlen(input);
+	if (input[len - 1] == '\n') input[len - 1] = '\0';
+
+	const char* dels = " ,.-";
+	char* token = NULL;
+	char* word = strtok_s(input, dels, &token);
+	while (word != NULL)
+	{
+		if (*word == beg && word[strlen(word) - 1] == end)
+		{
+			std::cout << word << std::endl;
+		}
+		word = strtok_s(NULL, dels, &token);
+	}
+}
+
+char* read_line()
 {
 	int size = 256, old_size = 0;
 	char* buf = new char[size]{0};
@@ -23,21 +66,4 @@ char* read_input()
 		fgets(buf + old_size - 1, size - old_size + 1, stdin);
 	}
 	return buf;
-}
-
-int lab7()
-{
-	// read input
-	char beg, end;
-	std::cout << "enter beginning character: ";
-	std::cin >> beg;
-	std::cout << "enter ending character: ";
-	std::cin >> end;
-	char* input = read_input();
-	std::cout << input;
-	// parse string
-//	parse_string(input, beg, end);
-
-	_getch();
-	return 0;
 }
